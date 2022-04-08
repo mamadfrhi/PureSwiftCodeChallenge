76494470fcfc4931cdf0007748b125eb72f5c932
s434383 <mamad.frhi@gmail.com>
2022-04-09 01:06:28 +0200

    catDetail page views added

---
+++ Cat Facts/Scenes/CatDetails/CatDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CatDetailViewController.swift
+//  Cat Facts
+//
+//  Created by iMamad on 4/9/22.
+//
+
+import UIKit
+
+class CatDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("I'm detail VC")
+    }
+}
