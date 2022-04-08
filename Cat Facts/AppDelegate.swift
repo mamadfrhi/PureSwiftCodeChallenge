5a2a678d394ce71c18479c3f43bf74c724d2a262
s434383 <mamad.frhi@gmail.com>
2022-04-08 10:51:17 +0200

    Coordinator design pattern added

--- Cat Facts/AppDelegate.swift
+++ Cat Facts/AppDelegate.swift
@@ -11,9 +11,15 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator!
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Setup Coordinator
+        window = UIWindow(frame: UIScreen.main.bounds)
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator.start()
         return true
     }
     
--- Cat Facts/Scenes/AppCoordinator.swift
+++ Cat Facts/Scenes/AppCoordinator.swift
@@ -5,4 +5,38 @@
 //  Created by iMamad on 4/8/22.
 //
 
-import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    let window: UIWindow?
+    
+    lazy var rootViewController: UINavigationController = {
+        return UINavigationController(rootViewController: UIViewController())
+    }()
+    
+    let apiClient: ApiClient = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Content-Type": "application/json; charset=utf-8"]
+        let apiClient = ApiClient(configuration: configuration)
+        return apiClient
+    }()
+    
+    // MARK: - Coordinator
+    init(window: UIWindow?) {
+        self.window = window
+    }
+    
+    override func start() {
+        guard let window = window else {
+            return
+        }
+        
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
+    
+    override func finish() {}
+    
+}
