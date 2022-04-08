f0f9c3c87ba4cce57c88bc1268f0b1b1a2262afb
s434383 <mamad.frhi@gmail.com>
2022-04-08 09:51:25 +0200

    Initial structure of the project implemented.

---
+++ Cat Facts/Scenes/AppCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppCoordinator.swift
+//  Cat Facts
+//
+//  Created by iMamad on 4/8/22.
+//
+
+import Foundation
---
+++ Cat Facts/Scenes/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  Cat Facts
+//
+//  Created by iMamad on 4/8/22.
+//
+
+import Foundation
