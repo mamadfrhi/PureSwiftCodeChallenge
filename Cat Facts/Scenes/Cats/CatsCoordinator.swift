95bbfc22692040707a6343de520514ce57c2f870
s434383 <mamad.frhi@gmail.com>
2022-04-08 12:43:48 +0200

    cleaned indenting of the Cats.swift

--- Cat Facts/Scenes/Cats/CatsCoordinator.swift
+++ Cat Facts/Scenes/Cats/CatsCoordinator.swift
@@ -10,36 +10,36 @@ import UIKit
 //Instantiate ViewController’s & ViewModel’s
 
 class CatsCoordinator: Coordinator {
-  
+    
     // MARK: - Properties
-  
+    
     let rootNavigationController: UINavigationController
     
     let storyboard = UIStoryboard(name: "Cats", bundle: nil)
-
+    
     let apiClient: ApiClient
-
+    
     // MARK: VM / VC's
-
+    
     var catsViewModel: CatsViewModel {
         let catsService = CatsServices(apiClient: apiClient)
         let viewModel = CatsViewModel(service: catsService)
         viewModel.coordinatorDelegate = self
         return viewModel
     }
-
+    
     // MARK: - Coordinator
     init(rootNavigationViewController: UINavigationController, apiClient: ApiClient) {
         self.rootNavigationController = rootNavigationViewController
         self.apiClient = apiClient
     }
-
+    
     override func start() {
         let catsVC: CatsViewController = storyboard.instantiateViewController(withIdentifier: "Cats") as! CatsViewController
         catsVC.viewModel = catsViewModel
         rootNavigationController.setViewControllers([catsVC], animated: false)
     }
-
+    
     override func finish() {
         // Clean up any view controllers. Pop them of the navigation stack for example.
         // delegate?.didFinish(from: self)
@@ -47,20 +47,27 @@ class CatsCoordinator: Coordinator {
     
 }
 
+// MARK: - Navigation
+extension CatsCoordinator {
+//    func goToLocationSearch(searchType: LocationSearchType, from controller: UIViewController) {
+//        let viewController: LocationSearchViewController = storyboard.instantiateViewController()
+//        viewController.viewModel = locationSearchViewModel
+//        controller.present(viewController, animated: true, completion: nil)
+//    }
+}
 // MARK: - Coordinator Callback's
 extension CatsCoordinator//: SearchCoordinatorDelegate
 {
-
-//    func didFinish(from coordinator: SearchCoordinator) {
-//        removeChildCoordinator(coordinator)
-//    }
-
+    //    func didFinish(from coordinator: SearchCoordinator) {
+    //        removeChildCoordinator(coordinator)
+    //    }
+    
 }
 
 // MARK: - ViewModel Callback's
 extension CatsCoordinator//: SearchInputViewModelCoordinatorDelegate
 {
-//    func didSelectOrigin(from controller: UIViewController) {
-//        goToLocationSearch(searchType: .origin, from: controller)
-//    }
+    //    func didSelectOrigin(from controller: UIViewController) {
+    //        goToLocationSearch(searchType: .origin, from: controller)
+    //    }
 }
