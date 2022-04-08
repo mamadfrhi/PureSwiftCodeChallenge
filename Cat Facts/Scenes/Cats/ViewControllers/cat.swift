93f8596a3b72bba8be8d7a9347ef110e1b98f051
s434383 <mamad.frhi@gmail.com>
2022-04-08 13:09:51 +0200

    basics and abstractions of CatsViewModel added.

---
+++ Cat Facts/Scenes/Cats/ViewControllers/cat.swift
@@ -0,0 +1,14 @@
+//
+//  cat.swift
+//  Cat Facts
+//
+//  Created by iMamad on 4/8/22.
+//
+
+import Foundation
+
+
+struct Cat {
+    let text: String
+    let createdAt: Date
+}
--- Cat Facts/Scenes/Cats/ViewModels/CatsViewModel.swift
+++ Cat Facts/Scenes/Cats/ViewModels/CatsViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by iMamad on 4/8/22.
 //
 
-import Foundation
+import UIKit
 
 class CatsViewModel {
     
@@ -15,4 +15,45 @@ class CatsViewModel {
     init(service: CatsServices) {
         self.serevice = service
     }
+}
+
+
+protocol CatsViewModelType {
+
+    var viewDelegate: CatsViewModelViewDelegate? { get set }
+
+    // Data Source
+    var shouldShowHeader: Bool { get }
+
+    var headerText: String { get }
+
+    func numberOfItems() -> Int
+
+    func itemFor(row: Int) -> UITableViewCell // PlaceViewDataType
+
+    // Events
+    func add()
+
+    func delete(text: String)
+
+    func didSelectRow(_ row: Int, from controller: UIViewController)
+
+    func didSelectClose(from controller: UIViewController)
+
+}
+
+protocol CatsViewModelCoordinatorDelegate: class {
+
+    func didSelect(cat: Cat, from controller: UIViewController)
+    
+    func didSelectClose(from viewModel: CatsViewModel, from controller: UIViewController)
+
+}
+
+protocol CatsViewModelViewDelegate: class {
+
+    func updateScreen()
+    
+//    func updateState(_ state: ViewControllerState)
+
 }
