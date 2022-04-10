9354887a0ef40a91ffd26113bec2721290914d62
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-10 11:47:39 +0200

    CatCoreDataConvertor.swift file cleaned.

--- Cat Facts/CoreData/CatCoreDataConvertor.swift
+++ Cat Facts/CoreData/CatCoreDataConvertor.swift
@@ -12,19 +12,18 @@ class CatCoreDataConvertor {
     func giveMeCats(from catsNSManagedObjects: [NSManagedObject]) -> [Cat] {
         var cats : [Cat] = []
         for nsManagedObj in catsNSManagedObjects {
+            // unwrap
             if let id = nsManagedObj.value(forKey: "id"),
                let text = nsManagedObj.value(forKey: "text"),
                let createdAtObj = nsManagedObj.value(forKey: "createdAt") {
-                
+                // to string
                 let id = "\(id)"
                 let text = "\(text)"
                 let cratedAt = "\(createdAtObj)"
                 
-                
                 let cat = Cat(_id: id,
                               text: text,
                               createdAt: cratedAt)
-                
                 
                 cats.append(cat)
             }
