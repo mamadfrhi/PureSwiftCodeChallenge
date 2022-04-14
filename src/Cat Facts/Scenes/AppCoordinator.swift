4490b504c5b88772e2a10e5809c70f8cf7e30814
Mamad Farrahi <mamad.frhi@gmail.com>
2022-04-14 08:26:34 +0200

    made properties of AppCoordinator private.

--- src/Cat Facts/Scenes/AppCoordinator.swift
+++ src/Cat Facts/Scenes/AppCoordinator.swift
@@ -12,18 +12,18 @@ class AppCoordinator: Coordinator {
     // MARK: Properties
     let window: UIWindow?
     
-    var rootViewController: UINavigationController = {
+    private var rootViewController: UINavigationController = {
         return UINavigationController(rootViewController: UIViewController())
     }()
     
-    var apiClient: Network = {
+    private var apiClient: Network = {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = ["Content-Type": "application/json; charset=utf-8"]
         let apiClient = ApiClient(configuration: configuration)
         return apiClient
     }()
     
-    lazy var coreDataManager = CoreDataManager()
+    private var coreDataManager = CoreDataManager()
     
     // MARK: Coordinator
     init(window: UIWindow?) { self.window = window }
